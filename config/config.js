76955b61d9a9c3bfc7a761f27276661d3123eb76
Dragos Strugar <itsdragos@gmail.com>
2019-03-16 21:05:05 +0300

    feat(graphql): init graphql

---
+++ config/config.js
@@ -0,0 +1,5 @@
+export default {
+  ROOT_ENDPOINT: "/api",
+  API_VERSION: "v1",
+  GRAPHQL_ENDPOINT: "graphql"
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+import express from "express";
+import bodyParser from "body-parser";
+import graphqlHttp from "express-graphql";
+import { buildSchema } from "graphql";
+
+import config from "../config/config";
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(
+  `${config.ROOT_ENDPOINT}/${config.API_VERSION}/${config.GRAPHQL_ENDPOINT}`,
+  graphqlHttp({
+    schema: buildSchema(`
+      type RootQuery {
+        users: [String!]!
+      }
+
+      type RootMutation {
+        createUser(name: String): String
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      users: () => ["ezio", "daniil", "vlad"],
+      createUser: args => args.name
+    },
+    graphiql: true
+  })
+);
 
 app.listen(3000);
